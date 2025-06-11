ef89890aea4263f1e7829d6692944cf0cf45d44d
hitoshibito <2301189@s.asojuku.ac.jp>
2025-06-11 12:41:26 +0900

    ログイン時の初期値実装

--- short_bbs/login.php
+++ short_bbs/login.php
@@ -1,3 +1,19 @@
+<?php
+    $pdo = new PDO(
+        'mysql:host=mysql322.phy.lolipop.lan;
+        dbname=LAA1553892-kadai01;charset=utf8',
+        'LAA1553892',
+        '0730'
+    );
+    $userId = 1;
+
+    $stmt = $pdo->prepare("SELECT * FROM user WHERE id = ?");
+    $stmt->execute([$userId]);
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    $name = $row ? $row['username'] : '';
+    $password = $row ? $row['password'] : '';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,8 +25,10 @@
 <body>
     <h1>ログイン</h1>
     <form action="check.php" method="POST">
-        <p>ユーザー名：<input type="text" name="username" required></p>
-        <p>パスワード：<input type="password" name="password" required></p>
+        <p>ユーザー名：<input type="text" name="username" 
+                      value="<?php echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8'); ?>" required></p>
+        <p>パスワード：<input type="password" name="password" 
+                      value="<?php echo htmlspecialchars($password, ENT_QUOTES, 'UTF-8'); ?>" required></p>
         <button type="submit">ログイン</button>
     </form>
 </body>
